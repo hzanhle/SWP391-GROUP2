4db375ed8e1a7d654d328a554eb9fa96b00d6f1f
E99PLANT <qvuong23102004@gmail.com>
2025-10-13 23:08:41 +0700

    ....

---
+++ Backend/EV_Rental_System/UserService/DTOs/UserDetailDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace UserService.DTOs
+{
+    public class UserDetailDTO
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserName { get; set; }
+        public DriverLicenseDTO? DriverLicense { get; set; }
+        public CitizenInfoDTO? CitizenInfo { get; set; }
+    }
+}
