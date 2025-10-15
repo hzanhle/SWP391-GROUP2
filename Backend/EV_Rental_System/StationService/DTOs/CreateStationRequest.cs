5c83e6c65ee71af48ce9c172cc88b38adb252074
hatdekc <nguyenphamwork19@gmail.com>
2025-10-15 13:14:41 +0700

    fix/ update validation of stationservice

--- Backend/EV_Rental_System/StationService/DTOs/CreateStationRequest.cs
+++ Backend/EV_Rental_System/StationService/DTOs/CreateStationRequest.cs
@@ -5,11 +5,11 @@ namespace StationService.DTOs
     {
         [Required(ErrorMessage = "Tên trạm không được để trống.")]
         [StringLength(200, MinimumLength = 5, ErrorMessage = "Tên trạm phải từ 5 đến 200 ký tự.")]
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         [Required(ErrorMessage = "Vị trí trạm không được để trống.")]
         [StringLength(500, MinimumLength = 10, ErrorMessage = "Vị trí trạm phải từ 10 đến 500 ký tự.")]
-        public string Location { get; set; }
+        public required string Location { get; set; }
         public int? ManagerId { get; set; } // User với role là Employee
     }
 }
