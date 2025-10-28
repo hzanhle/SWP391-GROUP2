f6142b7443108caa28bb409f031ce9c7e5cf4223
E99PLANT <qvuong23102004@gmail.com>
2025-10-28 10:25:14 +0700

    Add LicensePlate attribute for Vehicle object

--- Backend/EV_Rental_System/TwoWheelVehicleService/DTOs/VehicleRequest.cs
+++ Backend/EV_Rental_System/TwoWheelVehicleService/DTOs/VehicleRequest.cs
@@ -12,5 +12,10 @@ namespace TwoWheelVehicleService.DTOs
         [StringLength(30, ErrorMessage = "Màu xe không được vượt quá 30 ký tự")]
         public string Color { get; set; }
 
+        [RegularExpression(@"^[0-9]{2}[A-Z]{1,2}[0-9]-[0-9]{3,4}(\.[0-9]{2})?$",
+    ErrorMessage = "Biển số xe không hợp lệ. Ví dụ: 59X3-123.45 hoặc 30H1-5678")]
+        public string LicensePlate { get; set; }
+
+
     }
 }
